f4f933ea08ff63b8ccc0a4dd21a0d15a9b455577
arist76 <surafelfikru76@gmail.com>
2023-12-19 21:38:48 +0300

    add frontend and complete signup

---
+++ frontend/pdf_chat/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "../app.css";
+
+
+    import { afterNavigate } from "$app/navigation";
+
+    afterNavigate(() => {
+        HSStaticMethods.autoInit();
+    });
+</script>
+  
+ <slot />
