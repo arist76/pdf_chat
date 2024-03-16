89ea9e83ca7fc2a619b6355e0188c29f81887e75
surafel fikru <surafelfikru76@gmail.com>
2024-03-16 09:36:03 +0300

    update backend

--- backend/pdf_chat/forum/serializers.py
+++ backend/pdf_chat/forum/serializers.py
@@ -1,20 +1,32 @@
+from typing_extensions import ReadOnly
 from rest_framework import serializers
 from django.utils.text import slugify
 from forum.models import Room, ChatMessage
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    
     def to_internal_value(self, data):
         data["title_slug"] = slugify(data["title"])
         return super().to_internal_value(data)
+
     class Meta:
         model = Room
         fields = "__all__"
         read_only_fields = ["title_slug", "views", "upvotes", "downvotes", "owner"]
 
+
 class RoomChatMessageSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    room = RoomSerializer(read_only=True)
+
     class Meta:
         model = ChatMessage
         fields = "__all__"
-        read_only_fields = ["upvotes", "downvotes", "date", "room", "user"]
+        read_only_fields = ["upvotes", "downvotes", "date", "user"]
--- backend/pdf_chat/pdf_chat/urls.py
+++ backend/pdf_chat/pdf_chat/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
@@ -22,22 +23,28 @@ from rest_framework import permissions
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Chat PDF API",
-      default_version='v0',
-      description="Chat PDF API (development)",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Chat PDF API",
+        default_version="v0",
+        description="Chat PDF API (development)",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
- 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path("/", include("chat.urls")),
-    path("forum/", include("forum.urls")), 
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+    path("", include("chat.urls")),
+    path("forum/", include("forum.urls")),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
